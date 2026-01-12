2ca6eedbda4039a0facd8454a8ae1f90dabdf858
okanzen1 <okanozen1@hotmail.com>
2026-01-12 07:24:01 +0300

    image test

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -3,14 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Image;
+use Illuminate\Support\Facades\Response;
 
 class ImageController extends Controller
 {
     public function show(Image $image)
     {
-        $fullPath = base_path('../tripspoiler-admin/storage/app/private/' . $image->path);
+        $fullPath = storage_path('app/private/' . $image->path);
 
-        abort_unless(is_file($fullPath), 404);
+        abort_unless(file_exists($fullPath), 404);
 
         return response()->file($fullPath);
     }
