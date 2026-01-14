82ad2a23a00d6480eab1cc3efa0765657fe67fa7
okanzen1 <okanozen1@hotmail.com>
2026-01-14 04:12:44 +0300

    SiteMap

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,9 @@ use App\Http\Controllers\ImageController;
 use App\Http\Controllers\SearchController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CityController;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
+
 
 Route::get('/cities/{slug}-{id}', [CityController::class, 'show'])->name('cities.show');
 
@@ -45,3 +48,12 @@ Route::get('/search', [SearchController::class, 'index'])->name('search.index');
 
 Route::get('/media/{image}', [ImageController::class, 'show'])->name('images.view');
 Route::view('/privacy', 'other.privacy');
+
+Route::get('/sitemap-generate', function () {
+    Sitemap::create()
+        ->add(Url::create('/'))
+        ->writeToFile(public_path('sitemap.xml'));
+
+    return 'Sitemap generated';
+});
+
