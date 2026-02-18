6a1833683f0fe50cd3dc1e281e4198f2ebe5c30a
okanzen1 <okanozen1@hotmail.com>
2026-02-18 03:16:42 +0300

    Aktivite düzenlendi

--- resources/views/activities/show.blade.php
+++ resources/views/activities/show.blade.php
@@ -76,7 +76,7 @@
     </section>
 
     {{-- MAIN --}}
-    <section class="bg-white pb-28 lg:pb-0">
+    <section class="bg-white pb-0 lg:pb-0">
         <div class="max-w-6xl mx-auto px-4 py-12 grid grid-cols-1 lg:grid-cols-3 gap-12">
 
             {{-- LEFT --}}
