ebe0ea9062d6d7fda92e6aa3e35d885c013365cd
okanzen1 <okanozen1@hotmail.com>
2026-03-15 17:18:04 +0300

    test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,8 @@
     <link rel="apple-touch-icon" href="{{ asset('apple-touch-icon.png') }}">
 
     <link rel="manifest" href="{{ asset('site.webmanifest') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css">
+
 
 
     <title>@yield('title', 'TripSpoiler')</title>
